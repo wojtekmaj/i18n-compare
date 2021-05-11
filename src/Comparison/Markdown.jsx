781f6d24862d13131205a4d97bc84a72c38034e5
Wojciech Maj <kontakt@wojtekmaj.pl>
2021-05-11 10:23:03 +0200

    Update react-markdown to 6.0.0

---
+++ src/Comparison/Markdown.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+import ReactMarkdown from 'react-markdown';
+
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
+import rehypeSanitize from 'rehype-sanitize';
+
+export default function Markdown({ children }) {
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      rehypePlugins={[rehypeRaw, rehypeSanitize]}
+    >
+      {/* Have to change &shy; into <wbr /> as React-Markdown has issues rendering these */}
+      {children.replace(/&shy;/g, '<wbr />')}
+    </ReactMarkdown>
+  );
+}
+
+Markdown.propTypes = {
+  children: PropTypes.string,
+};
--- src/Comparison/Preview.jsx
+++ src/Comparison/Preview.jsx
@@ -3,11 +3,7 @@ import PropTypes from 'prop-types';
 
 import './Preview.less';
 
-const ReactMarkdown = lazy(() => new Promise((resolve, reject) => {
-  import(/* webpackChunkName: "react-markdown" */ 'react-markdown/with-html')
-    .then((result) => resolve(result.default ? result : { default: result }))
-    .catch(reject);
-}));
+const Markdown = lazy(() => import(/* webpackChunkName: "react-markdown" */ './Markdown'));
 
 export default function Preview({ textSource }) {
   return (
@@ -15,11 +11,9 @@ export default function Preview({ textSource }) {
       <h3>Preview</h3>
       <div className="Preview__body">
         <Suspense fallback={<p>Loading preview...</p>}>
-          <ReactMarkdown
-            // Have to change &shy; into <wbr /> as React-Markdown has issues rendering these
-            source={textSource.replace(/&shy;/g, '<wbr />')}
-            escapeHtml={false}
-          />
+          <Markdown>
+            {textSource}
+          </Markdown>
         </Suspense>
       </div>
     </div>
