03665eefc612dcff76070a3ec31a5148c1959917
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-10-24 16:21:56 +0200

    Remove remark-gfm

--- src/Comparison/Markdown.jsx
+++ src/Comparison/Markdown.jsx
@@ -2,13 +2,12 @@ import PropTypes from 'prop-types';
 
 import ReactMarkdown from 'react-markdown';
 
-import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import rehypeSanitize from 'rehype-sanitize';
 
 export default function Markdown({ children }) {
   return (
-    <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw, rehypeSanitize]}>
+    <ReactMarkdown rehypePlugins={[rehypeRaw, rehypeSanitize]}>
       {/* Have to change &shy; into <wbr /> as React-Markdown has issues rendering these */}
       {children.replace(/&shy;/g, '<wbr />')}
     </ReactMarkdown>
