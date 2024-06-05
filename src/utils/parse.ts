e2a24d0723818bd120bd54170a8ea138287aadfb
Wojciech Maj <kontakt@wojtekmaj.pl>
2024-06-05 11:32:57 +0200

    Fixes

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -6,7 +6,6 @@ export default function parse(str?: string): Record<string, string> | null {
   try {
     return JSON.parse(str);
   } catch (error) {
-    // eslint-disable-next-line no-console
     console.error(error);
     return null;
   }
